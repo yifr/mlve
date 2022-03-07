6e62f1828ea08e4df868aa6da257009b66aeb86b
Yoni Friedman <yoniifriedman@gmail.com>
2022-03-07 17:49:14 -0500

    working detection UI

--- experiments/app.py
+++ experiments/app.py
@@ -14,56 +14,61 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
 # mongo = PyMongo(app)
 
 
-def generate_probe_location(masks, probe_contact):
-    if probe_contact:
-        y, x = np.where(masks)
+def generate_probe_location(masks, probe_touching):
+    if probe_touching:
+        mask_val = np.random.choice(np.unique(masks))
+        mask = masks == mask_val
+
+        y, x = np.where(mask)
+        possible_locations = [loc for loc in zip(x, y)]
+        probe_idx = np.random.choice(range(len(possible_locations)))
+        loc = possible_locations[probe_idx]
+        return [int(l) for l in loc], mask
     else:
         y, x = np.where(masks == 0)
+        y_buffer, x_buffer = np.where(masks)
+        possible_locations = [loc for loc in zip(x, y)]
+        while True:
+            probe_idx = np.random.choice(range(len(possible_locations)))
+            loc = possible_locations[probe_idx]
 
-    possible_locations = [loc for loc in zip(x, y)]
-    probe_idx = np.random.choice(range(len(possible_locations)))
-    loc = possible_locations[probe_idx]
-    return [int(l) for l in loc]
+            # Avoid overlapping edge of probe with shape
+            for y_b, x_b in zip(y_buffer, x_buffer):
+                if (
+                    np.sqrt((loc[1] - y_b) ** 2) > 2.5
+                    and np.sqrt((loc[0] - x_b) ** 2) > 2.5
+                ):
+                    return [int(l) for l in loc], None
 
 
-def get_trial_data(experiment_type, domain, batch):
-    scenes = []
-    print(experiment_type, domain, batch)
-    if config.LOCAL_IMAGES:
+# @app.route("/get_probe_location", methods=["POST"])
+def get_probe_location(experiment_type, domain, batch, scene_index, image_index):
+
+    if experiment_type == "detection":
         if domain == "static":
-            trial_data = []
-            for scene in glob.glob(config.BASE_IMAGE_URLS):
-                image_url = os.path.join(scene, "images", "Image0001.png")
-                mask_url = os.path.join(scene, "masks", "Image0001.png")
-                masks = np.array(Image.open(mask_url).convert("L"))
+            scene = glob.glob(config.BASE_IMAGE_URLS)[scene_index]
+            mask_url = os.path.join(scene, "masks", f"Image{image_index:04d}.png")
+            masks = np.array(Image.open(mask_url).convert("L"))
 
-                # Generate probe location
-                probe_contact = (
-                    np.random.rand() >= 0.5
-                )  # TO-DO Evenly Balance out each experiment?
-                probe_location = generate_probe_location(masks, probe_contact)
+            # Generate probe location
+            probe_touching = (
+                np.random.rand() >= 0.5
+            )  # TO-DO Evenly Balance out each experiment?
 
-                # Compute bounding boxes
-                bounding_boxes = []
-                for mask_val in np.unique(masks):
-                    mask_y, mask_x = np.where(masks == mask_val)
-                    x_min = int(np.min(mask_x))
-                    x_max = int(np.max(mask_x))
-                    y_min = int(np.min(mask_y))
-                    y_max = int(np.max(mask_y))
-                    coords = ((x_min, y_min), (x_max, y_max))
-                    bounding_boxes.append(coords)
+            probe_location, mask = generate_probe_location(masks, probe_touching)
+            # Compute bounding boxes
+            if mask is not None:
+                mask_y, mask_x = np.where(mask)
+                x_min = int(np.min(mask_x))
+                x_max = int(np.max(mask_x))
+                y_min = int(np.min(mask_y))
+                y_max = int(np.max(mask_y))
+                coords = ((x_min, y_min), (x_max, y_max))
+                bounding_box = coords
+            else:
+                bounding_box = []
 
-                trial = {
-                    "image_url": image_url,
-                    "probe_location": probe_location,
-                    "probe_contact": probe_contact,
-                    "bounding_boxes": bounding_boxes,
-                }
-
-                trial_data.append(trial)
-
-    return trial_data
+            return probe_location, probe_touching, bounding_box
 
 
 @app.route("/get_trial_data", methods=["POST"])
@@ -72,9 +77,32 @@ def trial_data_wrapper():
     experiment_type = data.get("experiment")
     domain = data.get("domain")
     batch = data.get("batch")
-    stimuli_index = data.get("stimuli_index")
-    trial_data = get_trial_data("detection", "static", 0)
+    if config.LOCAL_IMAGES:
+        if domain == "static":
+            trial_data = []
+            for scene_index, scene in enumerate(glob.glob(config.BASE_IMAGE_URLS)[:20]):
+                image_index = np.random.randint(1, 32)
+
+                image_url = os.path.join(scene, "images", f"Image{image_index:04d}.png")
+                probe_location, probe_touching, bounding_box = get_probe_location(
+                    experiment_type, domain, batch, scene_index, image_index
+                )
+                trial = {
+                    "image_url": image_url,
+                    "stimulus_index": (scene_index, image_index),
+                    "probe_location": probe_location,
+                    "probe_touching": probe_touching,
+                    "bounding_box": bounding_box,
+                }
+                trial_data.append(trial)
+                print(scene_index)
     return jsonify(trial_data)
+
+
+@app.route("/save_trial", methods=["POST"])
+def save_to_db():
+    data = request.form
+    user_id = data.get("user_id")
 
 
 @app.route("/", methods=["GET"])
