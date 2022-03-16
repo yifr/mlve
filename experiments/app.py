fef65ea79154eef74108be37dec021b5bd8af04c
Yoni Friedman <yoniifriedman@gmail.com>
2022-03-16 01:17:31 -0400

    added attention trials

--- experiments/app.py
+++ experiments/app.py
@@ -130,8 +130,15 @@ def trial_data_wrapper():
             d["texture"] = texture
             d["n_objs"] = n_objs
             d["scene"] = scene
+
+            # Serve shaded images for attention trials
+            if "ground_truth" in d["image_url"]:
+                image_target = "shaded"
+            else:
+                image_target = "images"
+
             d["image_url"] = os.path.join(
-                s3_root, d["image_url"], "images", f"Image{d['frame_idx']:04d}.png"
+                s3_root, d["image_url"], image_target, f"Image{d['frame_idx']:04d}.png"
             )
 
         np.random.seed(config.random_seed)
