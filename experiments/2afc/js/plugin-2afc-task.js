076cb666f8fbc9749ba17b9f1bd66d269a8456d1
Yoni Friedman <yoniifriedman@gmail.com>
2022-05-09 14:02:02 -0400

    get rid of prompt

--- experiments/2afc/js/plugin-2afc-task.js
+++ experiments/2afc/js/plugin-2afc-task.js
@@ -61,7 +61,7 @@ var jsPsych2afcResponse = (function (jspsych) {
           prompt: {
               type: jspsych.ParameterType.HTML_STRING,
               pretty_name: "Prompt",
-              default: "<p id='prompt'>Which shape is the dot touching? </p>",
+              default: null,
           },
           /** How long to show the stimulus. */
           stimulus_duration: {
