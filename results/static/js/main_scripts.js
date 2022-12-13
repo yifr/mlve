b9e896a032e8ce26e4bdd5a7aa0e62ca3e28bdd0
Yoni Friedman <yoniifriedman@gmail.com>
2022-12-13 14:51:31 -0500

    updated vis server

--- results/static/js/main_scripts.js
+++ results/static/js/main_scripts.js
@@ -1,26 +1,70 @@
-function get_collection_names() {
 
-    const response = fetch("/get_collection_names")
-    .then(function(response) {
-        data = response.json();
-        console.log(data);
-        return data;
+async function getExperimentData(imageURL, experiment) {
+    // Get data from server for this image URL
+    const response = await fetch(`/get_experiment_data?` 
+                + new URLSearchParams({imageURL: imageURL, experiment: experiment}))
+    return response.json();
+}
+
+function loadExperimentData(modal, imageURL, experiment_name){
+    getExperimentData(imageURL, experiment_name).then(function(data) {
+        /* Visualize data */
+        data = data["data"]
+        console.log(data)
+    })
+}
+
+function populateVisModal(modal_id, imageURL) {
+    var modal = $(modal_id);
+    getExperimentOpts(imageURL).then(function(data) {
+       /* populate list of experiments with data */
+       var experiment_opts = data["data"];
+       var experiment_list = modal.find(".experiment-list"); 
+       for (var i = 0; i < experiment_opts.length; i++) {
+            // Add a new line to the list with experiment name
+            var experiment_name = experiment_opts[i]
+            var new_line = (`<a class='dropdown-item' onclick='loadExperimentData(${modal}, ${imageURL}, ${experiment_name})'>` + experiment_name + "</a>");
+            // Insert html for list item
+            experiment_list.append(new_line);
+       }
     });
 }
 
-/* Functionality to build: 
-
-1. List collection names
-2. Get data from collection
-3. Display data in nice clean format
-*/
-
-function get_experiment_data(col_name) {
-    const response = fetch(`/get_experiment_data?` + new URLSearchParams({
-        col_name: col_name}))
-    .then(function(response) {
-        data = response.json();
-        console.log(data);
-        return data;
-    });
+async function getExperimentOpts(imageURL) {
+    // Get data from server for this image URL
+    const response = await fetch(`/get_experiment_opts?` 
+                + new URLSearchParams({imageURL: imageURL}))
+    return response.json();
 }
+
+$(document).ready(function() {
+    function filterImages(dataset) {
+        console.log("Filtering dataset: " + dataset)
+        images = $(".dataset-image");
+        for (var i = 0; i < images.length; i++) {
+            var image_id = images[i].id;
+            if (image_id.includes(dataset)) {
+                images[i].style.display = "block";
+            } else {
+                images[i].style.display = "none";
+            }
+        }
+    }
+
+    function updateCurrentDataset(dataset) {
+        dataset_text = $("#currentDataset");
+        dataset_text.text(dataset);
+    }
+
+    var current_dataset = "nsd";
+    updateCurrentDataset("NSD")
+    filterImages(current_dataset);
+    
+    $(".dataset-option").on("click", function() {
+        var selected = $(this).text()
+        updateCurrentDataset(selected)
+        dataset = selected.toLowerCase();
+        filterImages(dataset)
+        current_dataset = selected;
+    });
+})
--- results/vis-server.py
+++ results/vis-server.py
@@ -1,37 +1,83 @@
 import os
 import sys
+import json
 import pymongo
 import numpy as np
 import pandas as pd
 import logging 
+from bson import json_util
 
 sys.path.append("../")
 import cabutils
 from flask import Flask, render_template, request, jsonify
 
 CONN = pymongo.MongoClient("mongodb://localhost:27017/")
-DB = CONN["mlve_outputs"]
+DB = CONN["mlve_results"]
+
+"""
+Backend Server for Visualizations:
+
+ - We want to be able to visualize individual responses for trials on a given experiment
+ - For a given url, participant ID, condition, etc. we want to return trial data
+ - Backend should filter data based on given parameters and return relevant trial information
+ - Filters in db include:
+        "ID": obscured user ID, 
+        "imageURL": image URL, 
+        "gt": ground truth answer (if exists), 
+        "response": user response, 
+        "score": accuracy / correct (if exists), 
+        "probeLocation": probe location(s), 
+        "batchID": batch ID, 
+        "expName": experiment name, 
+        "iterationName": iteration name (not super useful),
+
+ - Front end design is something like:
+    Image URL > experiment type (expName) > ID / probeLocation / batchID 
+"""
 
 template_dir = 'templates'
 app = Flask(__name__, template_folder=template_dir)
+
 app.logger.setLevel(logging.DEBUG)
 
 @app.route('/')
 def index():
     app.logger.info("Serving index.html")
-    return render_template('index.html')
+    urls = DB["results"].distinct("imageURL")
+    filtered_urls = {"NSD": [], "Gestalt": [], "Hypersim": [], "TDW": []}
+    for url in urls:
+        if "nsd" in url:
+            filtered_urls["NSD"].append(url)
+        elif "gestalt_shapegen" in url:
+            filtered_urls["Gestalt"].append(url)
+        elif "hypersim_v3" in url:
+            filtered_urls["Hypersim"].append(url)
+        elif "tdw" in url:
+            filtered_urls["TDW"].append(url)
 
-@app.route('/get_collection_names', methods=['GET'])
-def get_collections():
-    col_list = DB.list_collection_names()
-    app.logger.debug(f"Fetched Collections: {col_list}")
-    return jsonify({"col_list": col_list})
-    
+    urls = filtered_urls 
+    return render_template('index.html', urls=urls)
+
+
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
+
 @app.route('/get_experiment_data', methods=['GET'])
 def get_experiment_data():
-    col_name = request.args.get('col_name')
-    app.logger.debug(f"Fetching data from collection {col_name}")
-    col = DB[col_name]
-    data = list(col.find())
-    app.logger.debug(f"Fetched data: {data[0]}")
-    return jsonify({"data": data})
+    imageURL = request.args.get('imageURL')
+    expName = request.args.get('experiment')
+    app.logger.debug(f"Fetching data for image URL: {imageURL} and experiment: {expName}")
+    col = DB["results"]
+    data = list(col.find({"imageURL": imageURL, "expName": expName}))
+    app.logger.debug(f"Fetched {len(data)} records")
+    return parse_json({"data": data})
+
+@app.route('/get_experiment_opts', methods=['GET'])
+def get_experiment_opts():
+    # return possible experiments for given image URL
+    url = request.args.get('imageURL')
+    app.logger.debug(f"Fetching data for image URL: {url}")
+    col = DB["results"]
+    experiments = list(col.distinct("expName", {"imageURL": url}))
+    app.logger.debug(f"Found {len(experiments)} distinct experiments for {url}")
+    return parse_json({"data": experiments})
